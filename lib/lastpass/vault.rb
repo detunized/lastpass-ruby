8ca8ec4e605ca00f3bdd8507244d13ea57c106f0
Dmitry Yakimenko <detunized@gmail.com>
2013-11-20 17:41:45 +0100

    Vault embryo

---
+++ lib/lastpass/vault.rb
@@ -0,0 +1,10 @@
+# Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
+# Licensed under the terms of the MIT license. See LICENCE for details.
+
+require_relative "fetcher"
+require_relative "parser"
+
+module LastPass
+    class Vault
+    end
+end
