9083227cd235220b4a99af4063c2cc1f198abed4
Dmitry Yakimenko <detunized@gmail.com>
2013-11-23 01:11:05 +0100

    should -> expect

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -12,34 +12,33 @@ describe LastPass::Parser do
 
     describe "parse" do
         it "returns Parser" do
-            @parser.should be_an_instance_of LastPass::Parser
+            expect(@parser).to be_instance_of LastPass::Parser
         end
 
         it "raises an exception for nil blob" do
-            lambda { LastPass::Parser.parse nil, @key }.should raise_error ArgumentError
+            expect { LastPass::Parser.parse nil, @key }.to raise_error ArgumentError
         end
 
         it "raises an exception for empty blob" do
-            lambda { LastPass::Parser.parse "", @key }.should raise_error ArgumentError
+            expect { LastPass::Parser.parse "", @key }.to raise_error ArgumentError
         end
 
         it "raises an exception for invalid blob" do
-            lambda { LastPass::Parser.parse "ABCD", @key }.should raise_error ArgumentError
+            expect { LastPass::Parser.parse "ABCD", @key }.to raise_error ArgumentError
         end
     end
 
     it "returns chunks as a hash" do
-        @parser.chunks.should be_an_instance_of Hash
+        expect(@parser.chunks).to be_instance_of Hash
     end
 
     it "contains chunks with correct structure" do
         @parser.chunks.each do |id, chunks|
-            id.should be_an_instance_of String
-            id.size.should == 4
-            id.should match /^[A-Z]{4}$/
+            expect(id).to be_instance_of String
+            expect(id).to match /^[A-Z]{4}$/
 
-            chunks.should be_an_instance_of Array
-            chunks.size.should > 0
+            expect(chunks).to be_instance_of Array
+            expect(chunks.size).to be > 0
         end
     end
 
@@ -54,6 +53,12 @@ describe LastPass::Parser do
     it "contains NMAC chunk" do
         check_single_chunk "NMAC", "8"
     end
+
+    #
+    # Helpers
+    #
+
+    private
 
     def check_single_chunk id, value
         @parser.chunks.keys.should include id
