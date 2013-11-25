f6baa0fddccf55e1e8a357491ba36c75fe15e21a
Dmitry Yakimenko <detunized@gmail.com>
2013-11-25 01:37:07 +0100

    Account

--- lib/lastpass.rb
+++ lib/lastpass.rb
@@ -1,6 +1,7 @@
 # Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
 # Licensed under the terms of the MIT license. See LICENCE for details.
 
+require "lastpass/account"
 require "lastpass/blob"
 require "lastpass/chunk"
 require "lastpass/exceptions"
