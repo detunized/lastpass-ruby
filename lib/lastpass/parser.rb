73f61073546d3e70f1dc0353925c179b328a6799
Dmitry Yakimenko <detunized@gmail.com>
2013-11-25 01:16:04 +0100

    Old code removed

--- lib/lastpass/parser.rb
+++ lib/lastpass/parser.rb
@@ -50,62 +50,9 @@ module LastPass
             stream.read(4).unpack('N').first
         end
 
-        class << self
-            def parse blob, encryption_key
-                parser = Parser.new blob, encryption_key
-                parser.send :parse # to avoid exposing the private 'parse' method
-
-                parser
-            end
-        end
-
-        attr_reader :chunks
-
-        private
-
-        def initialize blob, encryption_key
-            @blob = blob
-            @encryption_key = encryption_key
-        end
-
-        # Does all the parsing
-        def parse
-            @chunks = parse_chunks extract_chunks decode_blob @blob
-        end
-
-        # Decodes the blob form base64 to raw
-        def decode_blob blob
-            if not String === blob
-                raise ArgumentError, 'Blob should be a string'
-            end
-
-            if blob[0, 4] != 'TFBB'
-                raise ArgumentError, 'Blob doesn\'t seem to be base64 encoded'
-            end
-
-            decode_base64 blob
-        end
-
-        # Splits the blob into the chunks grouped by id
-        def extract_chunks blob
-            chunks = Hash.new { |hash, key| hash[key] = [] }
-
-            StringIO.open blob do |stream|
-                while !stream.eof?
-                    chunk = read_chunk stream
-                    chunks[chunk[:id]] << chunk[:payload]
-                end
-            end
-
-            chunks
-        end
-
-        # Iterates over the chunks in the stream
-        def each_chunk stream
-            while !stream.eof?
-                yield read_chunk stream
-            end
-        end
+        #
+        # To be killed
+        #
 
         def parse_chunks raw_chunks
             parsed_chunks = {}
@@ -122,24 +69,6 @@ module LastPass
             end
 
             parsed_chunks
-        end
-
-        #
-        # IO
-        #
-
-        def read_chunk stream
-            # LastPass blob chunk is made up of 4-byte ID, 4-byte size and payload of that size
-            # Example:
-            #   0000: 'IDID'
-            #   0004: 4
-            #   0008: 0xDE 0xAD 0xBE 0xEF
-            #   000C: --- Next chunk ---
-            id = stream.read 4
-            size = read_uint32 stream
-            payload = stream.read size
-
-            {:id => id, :size => size, :payload => payload}
         end
 
         def read_item stream
