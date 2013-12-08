fe2d82c2feff3ef8d7abd5cc5ae841f46f99b3c2
Dmitry Yakimenko <detunized@gmail.com>
2013-12-09 00:59:19 +0100

    How to run example

--- example/example.rb
+++ example/example.rb
@@ -1,6 +1,9 @@
 # Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
 # Licensed under the terms of the MIT license. See LICENCE for details.
 
+# Run via top level rake file:
+# $ rake example
+
 require "lastpass"
 require "yaml"
 
