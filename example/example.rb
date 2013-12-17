c5d5b612acabeb4b361e81dfb5741c71f48f69e4
Dmitry Yakimenko <detunized@gmail.com>
2013-12-17 02:07:39 +0100

    GAuth support in the example

--- example/example.rb
+++ example/example.rb
@@ -12,7 +12,17 @@ credentials = YAML.load_file File.join File.dirname(__FILE__), "credentials.yaml
 username = credentials["username"]
 password = credentials["password"]
 
-vault = LastPass::Vault.open_remote username, password
+begin
+    # First try without a multifactor password
+    vault = LastPass::Vault.open_remote username, password
+rescue LastPass::LastPassIncorrectGoogleAuthenticatorCodeError => e
+    # Get the code
+    puts "Enter Google Authenticator code:"
+    code = gets.chomp
+
+    # And now retry with the code
+    vault = LastPass::Vault.open_remote username, password, code
+end
 
 vault.accounts.each_with_index do |i, index|
     puts "#{index + 1}: #{i.id} #{i.name} #{i.username} #{i.password} #{i.url} #{i.group}}"
