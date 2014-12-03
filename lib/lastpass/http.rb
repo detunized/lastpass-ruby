cdbdb4e7d04a639bf0952bfe599d05be2a4c9844
Chris D'Ambrosio <cdambrosio@gmail.com>
2014-12-03 10:29:19 -0800

    add file headers

--- lib/lastpass/http.rb
+++ lib/lastpass/http.rb
@@ -1,3 +1,6 @@
+# Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
+# Licensed under the terms of the MIT license. See LICENCE for details.
+
 module LastPass
   class HTTP
     include HTTParty
